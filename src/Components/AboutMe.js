694ccc48070ee0489095723c89386e0430fde7b5
kirirotha <kirirotha@gmail.com>
2020-10-28 16:25:56 -0500

    added contact

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -5,11 +5,11 @@ class AboutMe extends React.Component {
     render(){
         return (
             <div className="center-panel">
-                <h1>
+                <h2>
                     Hi, my name is Kiri Um. I am an aspiring full stack software engineer who also has a master's degree in geosciences. I was previously employed as a geophysicist and I am now in the process of making a career change to software development in the tech industry.<br/><br/>
 
                     I enjoy working on difficult projects and flexing my problem-solving skills. I am a hard-worker who learns quickly and can adapt to different environments easily. I love being creative with my work and would like to put my efforts towards building inspiring concepts one day.
-                </h1>
+                </h2>
             </div>
         )
     }
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,9 +5,9 @@ class Contact extends React.Component {
     render(){
         return (
             <div className="center-panel">
-                <h1>
-                    
-                </h1>
+                <h2>
+                    Feel free to contact me at am email kirirotha@gmail.com  or find me on LinkedIn. I would love to meet with you!
+                </h2>
             </div>
         )
     }
