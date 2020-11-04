923620c2cb6a57a60b67b963d5a218f633b23c34
kirirotha <kirirotha@gmail.com>
2020-11-04 10:38:01 -0600

    changed about me

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -8,7 +8,7 @@ class AboutMe extends React.Component {
                 <h2>
                     I am a full stack software engineer who also has a master's degree in geosciences. I was previously employed as a geophysicist and I am now in the process of making a career change to software development in the tech industry.<br/><br/>
 
-                    I enjoy working on difficult projects and flexing my problem-solving skills. I am a hard-worker who learns quickly and can adapt to different environments easily. I love being creative with my work and would like to put my efforts towards building inspiring concepts one day.<br/><br/>
+                    <br/><br/>
 
                     In my spare time, you can find me out rock climbing, mountain biking, hiking, camping or travelling with my wife and kids. 
                 </h2>
