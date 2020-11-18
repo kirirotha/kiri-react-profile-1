e5ec31603101bcfc38e993ade8cead2ba36f5f8b
kirirotha <kirirotha@gmail.com>
2020-11-18 10:04:53 -0600

    start readme

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -6,7 +6,15 @@ class AboutMe extends React.Component {
         return (
             <div className="center-panel">
                 <h2>
-                    I am a full stack software engineer who also has a master's degree in geosciences. I was previously employed as a geophysicist and I am now in the process of making a career change to software development in the tech industry.<br/><br/>
+                    {/* I am a full stack software engineer who also has a master's degree in geosciences. I was previously employed as a geophysicist and I am now in the process of making a career change to software development in the tech industry.<br/><br/> */}
+
+                    I began my career as a geologist looking for answers to the past. Now I’m software engineer trying to build solutions for the future.
+                    <br/><br/>
+                    In college, I climbed around the mountains of Montana, Wyoming and West Texas to find answers hidden in rock millions of years old. The projects that I worked on taught me how to be persistent with my goals and how to sometimes think abstractly because the answer you’re looking for is not always right in front of your face. I carry this attitude in life and I hope to apply this as I switch careers into the tech industry.
+                    <br/><br/>
+                    Transitioning from Earth sciences to software engineering was actually an easy fit for me. I found that computer code often seemed to mimic natural processes in the universe and a lot of Earth’s mechanisms could be explained in terms of loops, conditionals, functions and objects. I also recently completed a coding boot camp at Flatiron School and learned a lot of code in a very short period of time. Before that, I was already working with computer algorithms on a daily basis to manipulate seismic trace data in order to image subsurface structures. Now I can build those algorithms myself. 
+                    <br/><br/>
+                    I’m currently in search of a position in the tech industry where I can apply my hard-work ethic, creativity and problem-solving skills to contribute to amazing projects. Right now, I specialize in Ruby on Rails, Javascript and ReactJS, but I’m also currently learning Python and AWS cloud computing to add to my skillset.
 
                     <br/><br/>
 
