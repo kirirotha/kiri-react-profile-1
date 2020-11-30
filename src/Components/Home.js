26ce8a8595d263900a443c463712173b70722e15
kirirotha <kirirotha@gmail.com>
2020-11-30 16:34:02 -0600

    fixed deploy

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,7 +7,7 @@ class Home extends React.Component {
             <div className="center-panel">
                 <h1>
                     {`Hi, my name is Kiri Um. 
-                    I am a full-stack software engineer. `}
+                    I am a full-stack software engineer.   `}
                 </h1>
             </div>
         )
